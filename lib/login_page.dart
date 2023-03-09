3577fb873080a2010d32ececb5e42430190a9769
Yuvadi29 <adityatrivedi612@gmail.com>
2023-03-10 00:50:01 +0530

    Till Richtext Added

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -31,59 +31,135 @@ class _LoginPageState extends State<LoginPage> {
             children: [
               Text(
                 "Hello",
-                style: TextStyle(
-                  fontSize: 70,
-                  fontWeight: FontWeight.bold
-                ),
+                style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
               ),
-              Text(
-                "Sign in",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.pinkAccent[200],
-                ),
+              // Text(
+              //   "Sign in",
+              //   style: TextStyle(
+              //     fontSize: 20,
+              //     color: Colors.pinkAccent[200],
+              //   ),
+              // ),
+              SizedBox(
+                height: 50,
               ),
-              SizedBox(height: 50,),
               Container(
                 decoration: BoxDecoration(
-                  color: Colors.grey[20],
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 10,
-                      spreadRadius: 7,
-                      offset: Offset(1,1),
-                        color: Colors.grey.withOpacity(0.4)
-                    )
-                  ]
-                ),
+                    color: Colors.grey[20],
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(0.4))
+                    ]),
                 child: TextField(
                   decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 1.0
-                      )
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 1.0,
-                      )
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30)
-                    )
-                  ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1.0,
+                          )),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30))),
                 ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[20],
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 10,
+                          spreadRadius: 7,
+                          offset: Offset(1, 1),
+                          color: Colors.grey.withOpacity(0.4))
+                    ]),
+                child: TextField(
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1.0)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1.0,
+                          )),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Text(
+                    "Sign in",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.pinkAccent[200],
+                    ),
+                  ),
+                ],
               )
             ],
           ),
-        )
+        ),
+        SizedBox(
+          height: 70,
+        ),
+        Container(
+          width: w * 0.5,
+          height: h * 0.08,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              image: DecorationImage(
+                  image: AssetImage("img/loginbtn.png"), fit: BoxFit.cover)),
+          child: Center(
+            child: Text(
+              "SignIn",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: w*0.2),
+        RichText(text: TextSpan(
+          text: "No Account!  ",
+          style: TextStyle(
+            color: Colors.grey[500],
+            fontSize: 20,
+          ),
+          children: [
+            TextSpan(
+              text: "Create",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              )
+            )
+          ]
+        ))
       ],
-    )
-    );
+    ));
   }
 }
